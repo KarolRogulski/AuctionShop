c821fcbf6d614d4e8041cecde7310b772db05294
Karol Rogulski <karol.rogulski@vp.pl>
2019-03-17 14:32:49 +0100

    OpinionDao implementation which passed tests

--- src/main/java/com/auction/AuctionShop/repositories/OpinionDaoImpl.java
+++ src/main/java/com/auction/AuctionShop/repositories/OpinionDaoImpl.java
@@ -1,22 +1,27 @@
 package com.auction.AuctionShop.repositories;
 
-import org.hibernate.query.Query;
-
-import java.util.Set;
-
+import com.auction.AuctionShop.domain.Opinion;
+import com.auction.AuctionShop.domain.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.auction.AuctionShop.domain.Opinion;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Repository
 @Transactional
 public class OpinionDaoImpl implements OpinionDao {
 
 	private SessionFactory sessionFactory;
+    private static final Logger log = LogManager.getLogger(OpinionDaoImpl.class);
 
 	@Autowired
 	public OpinionDaoImpl(SessionFactory sessionFactory) {
@@ -29,11 +34,14 @@ public class OpinionDaoImpl implements OpinionDao {
 
 	@Override
 	public void save(Opinion opinion) {
-		getSession().save(opinion);
+	    log.info("Saving opinion with id: " + opinion.getId());
+		getSession().persist(opinion);
 	}
-	
+
+	// Return opinion by given id
 	@Override
 	public Opinion findById(long id) {
+	    log.info("Get opinion with id: " + id);
 		Query query = getSession().createQuery(
 					"from Opinion " +
 					"where id= :id");
@@ -42,26 +50,76 @@ public class OpinionDaoImpl implements OpinionDao {
 		return opinion;
 	}
 
+	//Return all opinions written by user with given id
 	@Override
-	public Opinion findByOwnerId(long id) {
-		return null;
+	public List<Opinion> findByAuthorId(long id) {
+		CriteriaBuilder builder = getSession().getCriteriaBuilder();
+		CriteriaQuery<Opinion> criteria = builder.createQuery(Opinion.class);
+		Root<Opinion> root = criteria.from(Opinion.class);
+
+		Join<Opinion, User> userJoin = root.join("opinionAuthor", JoinType.INNER);
+		List<Predicate> predicates = new ArrayList<>();
+
+		predicates.add(builder.equal( userJoin.get( "id" ), id));
+
+		criteria.where(
+				builder.and(predicates.toArray(new Predicate[predicates.size()]))
+		);
+		List<Opinion> opinionsList = getSession().createQuery( criteria )
+				.getResultList();
+		log.info("Returned " + opinionsList.size() + "opinions where authorId: " + id);
+		return opinionsList;
+	}
+
+	//Return all opinions
+	@Override
+    public List<Opinion> getAll(){
+	    log.info("Get all opinions");
+	    Query query = getSession().createQuery("from Opinion");
+	    List<Opinion> opinionList = query.list();
+	    return opinionList;
+    }
+
+    //Return all opinions for user with given id
+	@Override
+	public List<Opinion> findByUserId(long id) {
+		CriteriaBuilder builder = getSession().getCriteriaBuilder();
+		CriteriaQuery<Opinion> criteria = builder.createQuery(Opinion.class);
+		Root<Opinion> root = criteria.from(Opinion.class);
+
+		Join<Opinion, User> userJoin = root.join("user", JoinType.INNER);
+		List<Predicate> predicates = new ArrayList<>();
+
+		predicates.add(builder.equal( userJoin.get( "id" ), id));
+
+		criteria.where(
+				builder.and(predicates.toArray(new Predicate[predicates.size()]))
+		);
+		List<Opinion> opinionsList = getSession().createQuery( criteria )
+				.getResultList();
+		log.info("Returned " + opinionsList.size() + "opinions where user Id: " + id);
+		return opinionsList;
 	}
 
 	@Override
-	public Set<Opinion> findByUserId(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(Opinion opinion) {
-		// TODO Auto-generated method stub
-		
+	public void update(Opinion updatedOpinion) {
+		log.info("Updating opinion with id: " + updatedOpinion.getId());
+		Opinion opinionFromDB = this.findById(updatedOpinion.getId());
+		opinionFromDB.setRate(updatedOpinion.getRate());
+		opinionFromDB.setTitle(updatedOpinion.getTitle());
+		opinionFromDB.setDescription(updatedOpinion.getDescription());
 	}
 
 	@Override
 	public void delete(Opinion opinion) {
-		// TODO Auto-generated method stub
-		
+		CriteriaBuilder builder = getSession().getCriteriaBuilder();
+		CriteriaDelete<Opinion> criteria = builder.createCriteriaDelete(Opinion.class);
+		Root<Opinion> root = criteria.from(Opinion.class);
+
+		criteria.where(builder.equal(root.get("id"), opinion.getId()));
+
+		Query<Opinion> query = getSession().createQuery(criteria);
+		query.executeUpdate();
+		log.info("Deleted opinion with id: " + opinion.getId());
 	}
 }
