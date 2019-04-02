382a2f40813b4542baa796f2ef06b0c609172d55
Karol Rogulski <karol.rogulski@vp.pl>
2019-04-02 23:26:29 +0200

    Test data with opinions for DB

---
+++ src/test/resources/opinion-test-data.sql
@@ -0,0 +1,5 @@
+insert into OPINION(opinion_id, rate, title, description, user_id, author_id)
+            values (1, 2.3, 'someTitle2', 'someDescription2', 2, 3),
+                   (2, 4.5, 'someTitle3', 'someDescription3', 3, 2);
+
+
